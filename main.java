91e8a8907a5d313ddbd47347cc2eda31d2693560
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-03-29 12:23:19 +0100

    done

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] l = new int[]{1,2,1};
+        System.out.println(Arrays.toString(solution.getConcatenation(l)));
+    }
+}
